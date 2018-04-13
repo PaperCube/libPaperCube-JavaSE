9c60797b6762cb5a2c704628c59fd562b56f4215
PaperCube <imzhy@hotmail.com>
2018-04-13 20:23:27 +0800

    SimpleLogger: tag is now an empty string instead of null when tag is unspecified

--- SimpleLocalLogger/src/studio/papercube/library/simplelogger/Logger.kt
+++ SimpleLocalLogger/src/studio/papercube/library/simplelogger/Logger.kt
@@ -12,23 +12,23 @@ abstract class Logger {
     abstract fun e(tag: String?, msg: String)
 
     open fun v(msg: String) {
-        v(null, msg)
+        v("", msg)
     }
 
     open fun i(msg: String) {
-        i(null, msg)
+        i("", msg)
     }
 
     open fun w(msg: String) {
-        w(null, msg)
+        w("", msg)
     }
 
     open fun e(msg: String) {
-        e(null, msg)
+        e("", msg)
     }
 
     open fun e(e: Throwable) {
-        e(null, e.detailedString)
+        e("", e.detailedString)
     }
 
     open fun e(tag: String? = null, msg: String? = null, e: Throwable) {
